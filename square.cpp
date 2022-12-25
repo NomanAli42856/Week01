82db31ce6ebaa01246a1b57c5ef49706dbb4a96e
NomanAli42856 <121042827+NomanAli42856@users.noreply.github.com>
2022-12-25 20:10:07 +0500

    Add files via upload

---
+++ square.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+main()
+{
+system("cls");
+cout << "     ###########################   "<< endl; 
+cout << "     #                         #   "<< endl;
+cout << "     #                         #   "<< endl;
+cout << "     #                         #   "<< endl;
+cout << "     #                         #   "<< endl;
+cout << "     #                         #   "<< endl;
+cout << "     #                         #   "<< endl;
+cout << "     #                         #  "<< endl;
+cout << "     #                         #  "<< endl;
+cout << "     ###########################  "<< endl;
+}
